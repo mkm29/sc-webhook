73c854d14d88402abde47ceb51287e465161081d
Mitch Murphy Murphy <mitch.murphy@gmail.com>
2022-06-26 16:29:24 -0400

    Updated gen-certs.sh script to inject TLS information into relevant manifests

--- dev/gen-certs.sh
+++ dev/gen-certs.sh
@@ -3,15 +3,15 @@
 openssl genrsa -out ca.key 2048
 
 openssl req -new -x509 -days 365 -key ca.key \
-  -subj "/C=AU/CN=simple-kubernetes-webhook"\
+  -subj "/C=AU/CN=security-webhook"\
   -out ca.crt
 
 openssl req -newkey rsa:2048 -nodes -keyout server.key \
-  -subj "/C=AU/CN=simple-kubernetes-webhook" \
+  -subj "/C=AU/CN=security-webhook" \
   -out server.csr
 
 openssl x509 -req \
-  -extfile <(printf "subjectAltName=DNS:simple-kubernetes-webhook.default.svc") \
+  -extfile <(printf "subjectAltName=DNS:security-webhook.default.svc") \
   -days 365 \
   -in server.csr \
   -CA ca.crt -CAkey ca.key -CAcreateserial \
@@ -19,14 +19,25 @@ openssl x509 -req \
 
 echo
 echo ">> Generating kube secrets..."
-kubectl create secret tls simple-kubernetes-webhook-tls \
+kubectl create secret tls security-webhook-tls \
   --cert=server.crt \
   --key=server.key \
   --dry-run=client -o yaml \
   > ./manifests/webhook/webhook.tls.secret.yaml
 
+ca_bundle=$(cat ca.crt | base64 | tr -d '\n')
+tls_crt=$(cat server.crt | base64 | tr -d '\n')
+tls_key=$(cat server.key | base64 | tr -d '\n')
+
+
 echo
-echo ">> MutatingWebhookConfiguration caBundle:"
-cat ca.crt | base64 | fold
+echo ">> Updating webhook configurations..."
+sed -i "s/_CABUNDLE_/$ca_bundle/g" ./manifests/cluster-config/mutating.config.yaml
+sed -i "s/_CABUNDLE_/$ca_bundle/g" ./manifests/cluster-config/validating.config.yaml
+
+echo
+echo ">> Generating kube secrets..."
+sed -i "s/_TLSCRT_/$tls_crt/g" ./manifests/webhook/webhook.tls.secret.yaml
+sed -i "s/_TLSKEY_/$tls_key/g" ./manifests/webhook/webhook.tls.secret.yaml
 
 rm ca.crt ca.key ca.srl server.crt server.csr server.key
